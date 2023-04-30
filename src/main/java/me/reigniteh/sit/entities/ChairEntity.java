bc7aa0dbf0119ef0b47af4de76ee1f9109691806
Reigniteh <reigniteh@devcaveteam.eu>
2023-04-30 16:51:12 +0200

    Fixed event registration

---
+++ src/main/java/me/reigniteh/sit/entities/ChairEntity.java
@@ -0,0 +1,39 @@
+package me.reigniteh.sit.entities;
+
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+
+public class ChairEntity {
+
+    private ArmorStand chair;
+
+    public void spawn(World world, Location location) {
+
+        this.chair = (ArmorStand) world.spawnEntity(location.add(0, -1.6, 0), EntityType.ARMOR_STAND);
+
+        this.chair.setGravity(false);
+        this.chair.setVisible(false);
+        this.chair.setInvulnerable(false);
+
+    }
+
+    public ArmorStand getChair() {
+
+        return this.chair;
+
+    }
+
+    public void attachPlayer(Player player) {
+
+        if(this.chair != null) {
+
+            this.chair.addPassenger(player);
+
+        }
+
+    }
+
+}
