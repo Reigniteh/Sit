c1b230205e6bffa30fd87e478d1a70e2b3792f4f
Reigniteh <reigniteh@devcaveteam.eu>
2023-04-30 16:54:41 +0200

    Registered events

--- src/main/java/me/reigniteh/sit/Sit.java
+++ src/main/java/me/reigniteh/sit/Sit.java
@@ -1,12 +1,16 @@
 package me.reigniteh.sit;
 
+import me.reigniteh.sit.commands.SitCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Sit extends JavaPlugin {
 
+    public static Sit instance;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        this.getCommand("sit").setExecutor(new SitCommand());
+        this.getServer().getPluginManager().registerEvents(new SitCommand(), this);
 
     }
 
@@ -14,4 +18,11 @@ public final class Sit extends JavaPlugin {
     public void onDisable() {
         // Plugin shutdown logic
     }
+
+    public static Sit getInstance() {
+
+        return instance;
+
+    }
+
 }
